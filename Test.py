48b82cc40535f0cc07b723e594eac04645c7eda5
Angel Todorov <angel.todorov@rewardslabs.onmicrosoft.com>
2022-09-19 22:28:46 +0300

    first commit

---
+++ Test.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+print("I am Mr. Robot")
+
+print("enter number a")
+a=float(input())
+
+print("enter number b")
+b=float(input())
+
+c=a+b
+
+print("sum is ")
+print(c)
+
+print("Good night")
